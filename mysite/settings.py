c54e551d24e0dcf1e8e71436a1657c5382c6976d
emburyj <josh@LAPTOP-PBSFF0FB.localdomain>
2022-07-27 17:26:05 -0700

    updated database location in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,10 +75,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mysite.db',#   BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    )
 }
 
 
