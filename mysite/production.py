1e67776753fc50d1586d83cb6736decdffdffa75
emburyj <josh@LAPTOP-PBSFF0FB.localdomain>
2022-08-11 05:28:30 -0700

    updated files to include new host addr

--- mysite/production.py
+++ mysite/production.py
@@ -2,5 +2,5 @@ from settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['35.89.200.68', 'localhost']
+ALLOWED_HOSTS = ['34.219.27.108', 'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static)
